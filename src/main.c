31fbdb3019c95f204b90e27e15036320d648a5a6
kazhida <kazhida@abplus.com>
2021-10-06 18:43:09 +0900

    separate keycode.rb from keyboard.rb

--- src/main.c
+++ src/main.c
@@ -20,6 +20,7 @@
 /* ruby */
 /* models */
 #include "ruby/app/models/core.c"
+#include "ruby/app/models/keycode.c"
 #include "ruby/app/models/keyboard.c"
 #include "ruby/app/models/rotary_encoder.c"
 #include "ruby/app/models/rgb.c"
@@ -173,6 +174,7 @@ int main() {
   mrbc_load_model(rgb);
   mrbc_load_model(buffer);
   mrbc_load_model(rotary_encoder);
+  mrbc_load_model(keycode);
   mrbc_load_model(keyboard);
   mrbc_create_task(usb_task, 0);
   mrbc_create_task(rgb_task, 0);
